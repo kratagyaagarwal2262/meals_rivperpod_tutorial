fb1bfc842fbcd35b4c890fd21765cef59d36538f
Kratagya Agarwal <kratagyaagarwal2262@gmail.com>
2024-08-19 20:45:10 +0530

    Small refinements

--- lib/provider/filters_provider.dart
+++ lib/provider/filters_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../data/dummy_data.dart';
 import '../enums/enums.dart';
 
 class FilterProviderListner extends StateNotifier<Map<Filter, bool>> {
@@ -19,4 +20,23 @@ class FilterProviderListner extends StateNotifier<Map<Filter, bool>> {
 final filterProvider =
     StateNotifierProvider<FilterProviderListner, Map<Filter, bool>>((ref) {
   return FilterProviderListner();
+});
+
+final availableMealsProvider = Provider((ref) {
+    final selectedFileters = ref.watch(filterProvider);
+  return dummyMeals.where((meal) {
+      if (selectedFileters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (selectedFileters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (selectedFileters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (selectedFileters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
 });
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/provider/favourite_provider.dart';
 import 'package:meals/provider/filters_provider.dart';
 import 'package:meals/provider/index_provider.dart';
@@ -9,21 +8,15 @@ import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
 
-import '../enums/enums.dart';
 
-
-class TabsScreen extends ConsumerStatefulWidget {
+class TabsScreen extends ConsumerWidget {
   const TabsScreen({super.key});
 
   @override
-  ConsumerState<TabsScreen> createState() {
-    return _TabsScreenState();
-  }
-}
+  Widget build(BuildContext context, WidgetRef ref) {
 
-class _TabsScreenState extends ConsumerState<TabsScreen> {
-
-  void _setScreen(String identifier) async {
+    
+    void setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
       Navigator.of(context).push(
@@ -35,24 +28,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final selectedFileters = ref.watch(filterProvider);
-    final availableMeals = dummyMeals.where((meal) {
-      if (selectedFileters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (selectedFileters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (selectedFileters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (selectedFileters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(availableMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
@@ -71,7 +47,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
         title: Text(activePageTitle),
       ),
       drawer: MainDrawer(
-        onSelectScreen: _setScreen,
+        onSelectScreen: setScreen,
       ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
